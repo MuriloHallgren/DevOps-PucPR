6618e148e826fa2ee718b990427af670d4fc9b7a
MuriloHallgren <murilohfonseca25@gmail.com>
2026-03-17 18:55:22 -0300

    Update main.py
    Alterei os numeros de opções.

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 tarefas = []
 #Lista de Tarefas To-Do
 while True:
-    print("\n1 - Adicionar tarefa")
-    print("2 - Lista de Tarefas")
-    print("3 - Excluir tarefa")
-    print("4 - Sair")
+    print("\n0 - Adicionar tarefa")
+    print("1 - Lista de Tarefas")
+    print("2 - Excluir tarefa")
+    print("3 - Sair")
     opcao = input("Digite o número de sua escolha: ")
 
     # Adicionar
-    if opcao == "1":
+    if opcao == "0":
         tarefa = input("Defina a tarefa: ")
         tarefas.append(tarefa)
 
     # Enumerar
-    elif opcao == "2":
+    elif opcao == "1":
         for i, t in enumerate(tarefas):
             print(f"{i} - {t}")
 
     # Deletar
-    elif opcao == "3":
+    elif opcao == "2":
         num = int(input("Número da tarefa: "))
         tarefas.pop(num)
 
-    elif opcao == "4":
+    elif opcao == "3":
         break
